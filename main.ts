279b83a56baf736d081f49350b758dc9ddc55fb6
Jonathan Van Hying <45504971+jvanhyn@users.noreply.github.com>
2025-01-23 16:08:14 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,26 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
     basic.showArrow(ArrowNames.South)
 })
+input.onButtonPressed(Button.A, function () {
+	
+})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
     basic.showArrow(ArrowNames.East)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     basic.showArrow(ArrowNames.North)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
     basic.showArrow(ArrowNames.West)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
 })
 joystickbit.initJoystickBit()
 basic.forever(function () {
